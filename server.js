4970520419525baff9e25be02985db49098c48bd
warllen castro <warllencs@gmail.com>
2018-07-22 03:03:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-
-app.use(myParser.urlencoded({extended:false}));
-app.use(myParser.json());
-
-//QUEUE
-app.post('/',function(req,res){ 
-  res.sed("ola");
+//SERVIDOR PRINCIPAL
+var io = require('socket.io')();
+io.on('connection', function(client){
+	//recebe cliente 1
+	client.on('luz', function (data) {
+		//repassa cliente 2
+		io.emit('luz_status', { status: '1' });
+	});
 });
-app.listen(portaPadrao);
+io.listen(3000);
+console.log("SERVER ON");
